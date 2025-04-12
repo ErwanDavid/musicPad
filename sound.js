ba71e65804a22d1e337f4510ea3a0658f3292073
ErwanDavid <erwan.dd@gmail.com>
2025-04-12 23:05:44 +0200

    bug fix

--- sound.js
+++ sound.js
@@ -1,13 +1,13 @@
 
-const filter1 = new Tone.Filter({type : "lowpass" ,frequency : 9000 ,rolloff : -12 ,Q : 0.42 ,gain : 0});
-const filter2 = new Tone.Filter({type : "lowpass" ,frequency : 9000 ,rolloff : -12 ,Q : 0.42 ,gain : 0});
+const filter1 = new Tone.Filter({type : "lowpass" ,frequency : 900 ,rolloff : -12 ,Q : 0.42 ,gain : 0});
+const filter2 = new Tone.Filter({type : "lowpass" ,frequency : 900 ,rolloff : -12 ,Q : 0.42 ,gain : 0});
 
 var nbrVoice1 = 0;
 var nbrVoice2 = 0;
 var freq1 = 440;
 var freq2 = 220;
 var freqtrig1 = 660;
-var freqtrig2 = 440;
+var freqtrig2 = 660;
 
 const synth1  = new Tone.PolySynth(Tone.Synth, {
     oscillator: {
@@ -23,53 +23,50 @@ const synth1  = new Tone.PolySynth(Tone.Synth, {
     }
   }).toDestination();
 
-Tone.Destination.chain(filter1);
-Tone.Destination.chain(filter2);
+Tone.Destination.chain(filter1, filter2);
 Tone.start()
 
 positionOSC1.on('change',function(v) {
     nbrVoice1 = nbrVoice1 +1;
-    freq1 = (660 * (v.y)) + 100;
-    freqcut1 = 2000 * (v.x + 0.8);
-    if (nbrVoice1 < 2) {
-        freqtrig1 = freq1
-        log_info("Go synth 1" + freq1);
-        synth1.triggerAttack(freqtrig1, Tone.now(), 1);
-    }
-    else{
-        synth1.set({ frequency: freq1 });
-        filter1.set({ frequency: freqcut1 });
-    }
+    noteOrfreq(synth1, filter1, nbrVoice1, v);
 })
 
 positionOSC2.on('change',function(w) {
     nbrVoice2 = nbrVoice2 +1;
-    freq2 = (330 * (w.y));
-    freqcut2 = 2000 * (w.x + 0.8);
-    if (nbrVoice2 < 2) {
-        freqtrig2 = freq2
-        log_info("Go synth 2"+ freq2);
-        synth2.triggerAttack(freqtrig2, Tone.now(), 1);
-    }
-    else{
-        synth2.set({ frequency: freq2 });
-        filter2.set({ frequency: freqcut2 });
-    }
+    noteOrfreq(synth2, filter2, nbrVoice2, w);
 })
 
 const OSC1Div1 = document.getElementById('OSC1');
 OSC1Div1.addEventListener('mouseup', () => {
-    log_info("Stop synth 1" + freqtrig1);
+    log_info("Stop synth 1 F:" + freqtrig1);
     synth1.triggerRelease(freqtrig1);
     nbrVoice1 = 0;
 });
 
 const OSC1Div2 = document.getElementById('OSC2');
 OSC1Div2.addEventListener('mouseup', () => {
-    log_info("Stop synth 1" + freqtrig2);
+    log_info("Stop synth 2 F:" + freqtrig2);
     synth2.triggerRelease(freqtrig2);
     nbrVoice2 = 0;
-});
+}); 
+
+function noteOrfreq(curSynth, curFilter, curVoices, pos) {
+    freq1 = (660 * (pos.y)) + 100;
+    freq1 = freq1.toFixed(2);
+    freqcut1 = 900 * (pos.x + 0.2);
+    freqcut1 = freqcut1.toFixed(2);
+    if (curVoices < 2) {
+        freqtrig1 = freq1
+        freqtrig2 = freq1
+        log_info("Go synth F:"  + freq1);
+        curSynth.triggerAttack(freqtrig1, Tone.now(), 1);
+    }
+    else{
+        log_info("Update synth F:"  + freq1  + " filter C:" + freqcut1);
+        curSynth.set({ frequency: freq1 });
+        curFilter.set({ frequency: freqcut1 });
+    }
+}
 
 function log_info(mytext) {
     console.log(mytext);
